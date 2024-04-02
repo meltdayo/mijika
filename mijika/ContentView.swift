1ba86ff59d1bb0b38e5fb86532f8d59796db4b92
takumi-1016 <tn1016recruit@gmail.com>
2024-04-02 23:33:42 +0900

    Initial Commit

---
+++ mijika/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mijika
+//
+//  Created by 中川　匠 on 2024/04/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mijika/mijikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mijikaApp.swift
+//  mijika
+//
+//  Created by 中川　匠 on 2024/04/02.
+//
+
+import SwiftUI
+
+@main
+struct mijikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
